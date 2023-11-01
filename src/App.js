da599c662db5b9b729760a4bdac9a97f860a6550
Bodin Todorovic <bodin.todorovic@endava.com>
2023-11-01 11:28:14 +0100

    Adding bar chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,160 @@
-import logo from './logo.svg';
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, Tooltip, DataLabel, BarSeries, LineSeries } from '@syncfusion/ej2-react-charts';
+import { dataSegmented } from './data/datasource';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+    return (
+      <ChartComponent
+        primaryXAxis={{
+          valueType: 'Category',
+          majorGridLines: {
+            width: 0,
+          },
+          visible: false,
+        }}
+        primaryYAxis={{
+          majorGridLines: {
+            width: 0,
+          },
+          visible: false,
+        }}
+        title='Position filled by'
+        legendSettings={{
+          padding: 10,
+          shapePadding: 10,
+          visible: true,
+          width: '100',
+          height: '200',
+          alignment: 'Near',
+          textWrap: 'Wrap',
+          maximumLabelWidth: 100,
+        }}
+        tooltip={{enable: true}}
+        chartArea={{ border: { width: 0 } }}
+        width='100%'
+        height='100%'
+      >
+        <Inject services={[BarSeries, Category, Legend, DataLabel, Tooltip]}/>
+        <SeriesCollectionDirective>
+          <SeriesDirective
+            type='Bar'
+            dataSource={dataSegmented[0]}
+            yName='amount'
+            xName='position'
+            name={dataSegmented[0][0].position}
+            pointColorMapping= 'point'
+            legendShape='Circle'
+            // columnSpacing={0.5}
+            // columnWidthInPixel={14}
+            marker={{
+                dataLabel: {
+                    visible: true,
+                    position: 'Top',
+                    font: { fontWeight: '600', color: '#ffffff' },
+                },
+            }}
+             columnWidth={1} columnSpacing={0}
+          ></SeriesDirective>
+          <SeriesDirective
+            type='Bar'
+            dataSource={dataSegmented[1]}
+            yName='amount'
+            xName='position'
+            name={dataSegmented[1][0].position}
+            pointColorMapping= 'point'
+            legendShape='Circle'
+            // columnSpacing={0.5}
+            // columnWidthInPixel={14}
+            marker={{
+                dataLabel: {
+                    visible: true,
+                    position: 'Top',
+                    font: { fontWeight: '600', color: '#ffffff' },
+                },
+            }}
+             columnWidth={1} columnSpacing={0}
+          ></SeriesDirective>
+          <SeriesDirective
+            type='Bar'
+            dataSource={dataSegmented[2]}
+            yName='amount'
+            xName='position'
+            name={dataSegmented[2][0].position}
+            pointColorMapping= 'point'
+            legendShape='Circle'
+            // columnSpacing={0.5}
+            // columnWidthInPixel={14}
+            marker={{
+                dataLabel: {
+                    visible: true,
+                    position: 'Top',
+                    font: { fontWeight: '600', color: '#ffffff' },
+                },
+            }}
+             columnWidth={1} columnSpacing={0}
+          ></SeriesDirective>
+          <SeriesDirective
+            type='Bar'
+            dataSource={dataSegmented[3]}
+            yName='amount'
+            xName='position'
+            name={dataSegmented[3][0].position}
+            pointColorMapping= 'point'
+            legendShape='Circle'
+            // columnSpacing={0.5}
+            // columnWidthInPixel={14}
+            marker={{
+                dataLabel: {
+                    visible: true,
+                    position: 'Top',
+                    font: { fontWeight: '600', color: '#ffffff' },
+                },
+            }}
+             columnWidth={1} columnSpacing={0}
+          ></SeriesDirective>
+          <SeriesDirective
+            type='Bar'
+            dataSource={dataSegmented[4]}
+            yName='amount'
+            xName='position'
+            name={dataSegmented[4][0].position}
+            pointColorMapping= 'point'
+            legendShape='Circle'
+            // columnSpacing={0.5}
+            // columnWidthInPixel={14}
+            marker={{
+                dataLabel: {
+                    visible: true,
+                    position: 'Top',
+                    font: { fontWeight: '600', color: '#ffffff' },
+                },
+            }}
+             columnWidth={1} columnSpacing={0}
+          ></SeriesDirective>
+          <SeriesDirective
+            type='Bar'
+            dataSource={dataSegmented[5]}
+            yName='amount'
+            xName='position'
+            name={dataSegmented[5][0].position}
+            pointColorMapping= 'point'
+            legendShape='Circle'
+            // columnSpacing={0.5}
+            // columnWidthInPixel={14}
+            marker={{
+                dataLabel: {
+                    visible: true,
+                    position: 'Top',
+                    font: { fontWeight: '600', color: '#ffffff' },
+                },
+            }}
+             columnWidth={1} columnSpacing={0}
+          ></SeriesDirective>
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    );
+};
 
 export default App;
---
+++ src/data/datasource.js
@@ -0,0 +1,32 @@
+export const PALETTE = {
+    yellow: "#fecb32",
+    dark_yellow: "#ff9f10",
+    red: "#E94649",
+    pink: "#ef53c3",
+    light_violet: "#cfbef0",
+    light_blue: "#64b5f6",
+    marine_blue: "#6FAAB0",
+    marine_green: "#88e4cc",
+    lemon_green: "#C4C24A",
+    gray: "#aaa",
+    dark_gray: "#2b3039",
+  };
+  
+  export const data = [
+    { position: "Asset", amount: 9, point: PALETTE.light_blue },
+    { position: "Organization", amount: 4, point: PALETTE.light_violet },
+    { position: "Component", amount: 7, point: PALETTE.yellow },
+    { position: "Unit", amount: 3, point: PALETTE.dark_yellow },
+    { position: "System", amount: 12, point: PALETTE.dark_gray },
+    { position: "Facility", amount: 2, point: PALETTE.marine_green },
+  ];
+
+  export const dataSegmented = [
+    [{ position: "Asset", amount: 9, point: PALETTE.light_blue }],
+    [{ position: "Organization", amount: 4, point: PALETTE.light_violet }],
+    [{ position: "Component", amount: 7, point: PALETTE.yellow }],
+    [{ position: "Unit", amount: 3, point: PALETTE.dark_yellow }],
+    [{ position: "System", amount: 12, point: PALETTE.dark_gray }],
+    [{ position: "Facility", amount: 2, point: PALETTE.marine_green }],
+  ];
+  
